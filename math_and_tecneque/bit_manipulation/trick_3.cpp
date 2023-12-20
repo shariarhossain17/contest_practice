d149722488b1e160ec54d8bd47e65a259a8a2d7c
shariar17e <shariarhossain23@gmail.com>
2023-12-21 00:31:24 +0600

    bit manipulation trick-3

---
+++ math_and_tecneque/bit_manipulation/trick_3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+
+    int result = a ^ b;
+
+    cout << result;
+}
