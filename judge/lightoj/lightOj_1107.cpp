1e7a5c2622cf0eec0ac05e426c3d396b6728c4db
shariar17e <shariarhossain23@gmail.com>
2024-01-09 22:27:16 +0600

    lightoj 1071

---
+++ judge/lightoj/lightOj_1107.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int t)
+{
+
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int m;
+    cin >> m;
+
+    bool flag = false;
+
+    cout << "Case " << t << ":\n";
+    while (m--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (x > 1 and y > y1 and x < x2 and y < y2)
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        };
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    int cnt = 0;
+    while (t--)
+    {
+        cnt++;
+        solve(cnt);
+    }
+
+    return 0;
+}
