efb541be64f66cc1959c08a6ebe82e1472b011d9
shariar17e <shariarhossain23@gmail.com>
2024-02-05 09:35:24 +0600

    maximize xor agter operation

---
+++ judge/leetcode/bit_manipulation/max_xor_after_operation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    int ans = 0;
+    for (int it : v)
+    {
+        ans |= it;
+    }
+
+    cout << ans;
+    return 0;
+}
