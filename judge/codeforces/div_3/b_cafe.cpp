ebd2db9ecc3c8278da3632d80792b6f368443b04
shariar17e <shariarhossain23@gmail.com>
2024-04-04 12:17:36 +0600

    div_3 problem

---
+++ judge/codeforces/div_3/b_cafe.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    if (log2(n) < k)
+    {
+        cout << n + 1 << "\n";
+    }
+    else
+    {
+        cout << pow(2, k) << "\n";
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
