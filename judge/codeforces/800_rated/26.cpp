c77509617272dfe278aecef9a0d47286dfc3cd21
shariar17e <shariarhossain23@gmail.com>
2024-04-19 19:10:24 +0600

    800 rated problem solved26

---
+++ judge/codeforces/800_rated/26.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int w = min(a, b) * 2;
+    int l = max(a, b);
+
+    if (w < l)
+        cout << l * l << endl;
+    else
+        cout << w * w << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
