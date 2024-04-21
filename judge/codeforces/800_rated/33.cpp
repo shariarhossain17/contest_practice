4f24b92e940a56614cb6f031fc4dacca554fea0c
shariar17e <shariarhossain23@gmail.com>
2024-04-21 23:20:15 +0600

    800 rated problem solved33

---
+++ judge/codeforces/800_rated/33.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    long double diff_double = (long double)n;
+    ll result = (ll)(diff_double / 2);
+
+    if (result % 2 == 0 && result != 0)
+        cout
+            << result - 1 << "\n";
+    else
+        cout << result << "\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
